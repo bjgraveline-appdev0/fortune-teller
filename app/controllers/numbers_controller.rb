e72fee919906e89b6d627a9f4ec38afae38a93c0
bjgraveline-appdev <bgraveline@chicagobooth.edu>
2023-01-26 02:08:52 +0000

    done

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -10,4 +10,16 @@ class NumbersController < ApplicationController
 
     render({ :template => "lottery_stuff/woohoo.html.erb"})
   end 
+
+  def losers
+    @hippo = Array.new
+
+    5.times do
+      giraffe = rand(1...100)
+
+      @hippo.push(giraffe)
+    end
+  
+    render({ :template => "lottery_stuff/darn.html.erb"})
+  end
 end
